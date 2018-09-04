e3245fe50042b22aa370cec39c2df85074c4d0d8
Abel Trotter <a_trotter0@hotmail.com>
2018-09-03 21:05:36 -0700

    Initial commit, build basic app outline

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Ticket from './Ticket';
+
+function TicketList() {
+  return(
+    <Ticket
+      location="3A"
+      names="Haley and Thato"
+      issue="Firebase will not save record"
+    />
+  );
+}
+
+export default TicketList;
